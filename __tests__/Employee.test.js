8019fcd51d2ebd898955588b7ce6e5e5f4fb101e
disha <mumudb@gmail.com>
2022-05-01 22:05:00 -0400

    add employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,3 +1,9 @@
-export class Employee {
-    name
-}
+const Employee = require("../lib/Employee.js");
+
+test("creates an employee", () => {
+  const employee = new Employee("fakeName", "fakeId", "fakeEmail");
+
+  expect(employee.name).toBe("fakeName");
+  expect(employee.id).toBe("fakeId");
+  expect(employee.email).toBe("fakeEmail");
+});
